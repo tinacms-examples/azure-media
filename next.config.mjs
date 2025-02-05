8a5a0d7439029b4026cdc97caa7bb91bde2144bd
JackDevAU <jpr178@live.com.au>
2025-02-05 14:08:04 +1000

    first: barebones starter

---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+
+export default {
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+      },
+      {
+        source: "/admin",
+        destination: "/admin/index.html",
+      },
+    ];
+  },
+}
